78d7f4ef66dbde76028cf9e8cf32b76918f58c60
GuHyeonWoo <ghw9174@gmail.com>
2022-03-28 12:32:14 +0900

    MakeModal.css 수정

--- src/modals/MakeModal.js
+++ src/modals/MakeModal.js
@@ -6,6 +6,7 @@ const MakeModal=(props)=>{
 
     var todayDate = new Date();
     const [today, settoday] = useState(todayDate);
+    const [checkHidden, setcheckHidden] = useState(false);
 
     const goToday=()=>{
         settoday(todayDate);
@@ -14,6 +15,43 @@ const MakeModal=(props)=>{
     const goTomorrow=()=>{
         var tomorrow = new Date(todayDate.setDate(todayDate.getDate() + 1));
         settoday(tomorrow);
+    };
+
+    const FormCheck=()=>{
+        return(
+            <div className='genderchoice'>
+                <p className='genderP'>
+                    성별 여부
+                </p>
+                <Form className='formchecks'>
+                    {['radio'].map((type) => (
+                        <div key={`inline-${type}`} className="mb-3">
+                        <Form.Check
+                            inline
+                            label="상관없음"
+                            name="group1"
+                            type={type}
+                            id={`inline-${type}-1`}
+                        />
+                        <Form.Check
+                            inline
+                            label="남자만"
+                            name="group1"
+                            type={type}
+                            id={`inline-${type}-2`}
+                        />
+                        <Form.Check
+                            inline
+                            label="여자만"
+                            name='group1'
+                            type={type}
+                            id={`inline-${type}-3`}
+                        />
+                        </div>
+                    ))}
+                </Form>
+            </div>
+        );
     };
 
     return(
@@ -38,8 +76,8 @@ const MakeModal=(props)=>{
                     <div className='subcontent'>
                         <Badge id='todaydate' bg="light" text="dark">{today.getFullYear()}년 {today.getMonth()+1}월 {today.getDate()}일</Badge>
                         <div className='buttonDate'>
-                            <Button onClick={goToday}>오늘</Button>
-                            <Button onClick={goTomorrow}>내일</Button>
+                            <Button variant="outline-dark" className='buttonDateLeft' onClick={goToday}>오늘</Button>
+                            <Button variant="outline-dark" className='buttonDateRight' onClick={goTomorrow}>내일</Button>
                         </div>
                     </div>
                 </div>
@@ -50,13 +88,11 @@ const MakeModal=(props)=>{
                     <div className="vr" />
                     <div className='subcontent'>
                         <div className='choiceHour'>
-                            <Form.Control placeholder={today.getHours()}/>
-                            <p>
-                                시
-                            </p>
+                            <Form.Control className='inputHour' placeholder={today.getHours()}/>
+                            <Badge className='timeP' bg='light' text='dark'>시</Badge>
                         </div>
                         <div className='choiceMinute'>
-                            <Form.Select defaultValue={(parseInt(today.getMinutes()/10+1)*10).toString()}>
+                            <Form.Select className='inputMinute' defaultValue={(parseInt(today.getMinutes()/10+1)*10).toString()}>
                                 <option>0</option>
                                 <option>10</option>
                                 <option>20</option>
@@ -64,9 +100,7 @@ const MakeModal=(props)=>{
                                 <option>40</option>
                                 <option>50</option>
                             </Form.Select>
-                            <p>
-                                분
-                            </p>
+                            <Badge className='timeP' bg='light' text='dark'>분</Badge>
                         </div>
                     </div>
                 </div>
@@ -76,60 +110,31 @@ const MakeModal=(props)=>{
                     </div>
                     <div className="vr" />
                     <div className='subcontent'> 
-                        <Form.Control placeholder="EX) 구미역 후문"/>
+                        <Form.Control className='inputDestination' placeholder="EX) 구미역 후문"/>
                     </div>
                 </div>
                 <div className='MakeOther'>
-                    <div className='subtitle'>
-                        참가자 설정
+                    <div className='subtitle' style={{whiteSpace: "pre-wrap"}}>
+                        참가자{"\n"}설정
                     </div>
                     <div className="vr" />
                     <div className='otherContent'>
-                        <Form.Check 
-                            type="switch"
-                            id="custom-switch"
-                            label="학교인증 여부"
-                            onChange={()=>console.log("yes")}
-                        />
-                        <div className='genderchoice'>
-                            <p>
-                                성별 여부
-                            </p>
-                            <Form>
-                                {['radio'].map((type) => (
-                                    <div key={`inline-${type}`} className="mb-3">
-                                    <Form.Check
-                                        inline
-                                        label="상관없음"
-                                        name="group1"
-                                        type={type}
-                                        id={`inline-${type}-1`}
-                                    />
-                                    <Form.Check
-                                        inline
-                                        label="남자만"
-                                        name="group1"
-                                        type={type}
-                                        id={`inline-${type}-2`}
-                                    />
-                                    <Form.Check
-                                        inline
-                                        label="여자만"
-                                        name='group1'
-                                        type={type}
-                                        id={`inline-${type}-3`}
-                                    />
-                                    </div>
-                                ))}
-                            </Form>
+                        <div className='univSwitch'>
+                            <Form.Check 
+                                type="switch"
+                                id="custom-switch"
+                                label="학교인증 여부"
+                                onChange={()=>{setcheckHidden(!checkHidden)}}
+                            />
                         </div>
+                        {checkHidden && <FormCheck />}
                     </div>               
                 </div>
             </Modal.Body>
 
             <Modal.Footer style={{backgroundColor: "#FFFCEE"}}>
-                <Button onClick={props.onHide}>Cancel</Button>
-                <Button onClick={props.onHide}>Make</Button>
+                <Button className='MakeButtonLeft' onClick={props.onHide}>Cancel</Button>
+                <Button className='MakeButtonRight' onClick={props.onHide}>Make</Button>
             </Modal.Footer>
         </Modal>
     );
