bcfdfd1fff1737fc8d407fcc06379ae71482c80e
GuHyeonWoo <ghw9174@gmail.com>
2022-04-03 14:03:34 +0900

    로그인 폼 수정

--- src/forms/LoginForm.js
+++ src/forms/LoginForm.js
@@ -1,30 +1,19 @@
-import {Form, Button} from 'react-bootstrap';
+import {Button} from 'react-bootstrap';
 import './LoginForm.css';
 
 const LoginForm =()=>{
+  
     return(
-        <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label className='loginlabel'>ID</Form.Label>
-                <Form.Control className='logininput' type="ID" placeholder="Enter ID" />
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label className='loginlabel'>Password</Form.Label>
-                <Form.Control className='logininput' type="password" placeholder="Password" />
-            </Form.Group>
-
-            <div className='ButtonGroup'>
-                <Button className='loginbutton' type="submit" href='/Main'
+        <div className='buttonGroup'>
+            <Button className='loginbutton' type="submit"
+                style={{margin: "5px", borderColor: "#515151" ,backgroundColor: "#515151"}}>
+                SignUp
+            </Button>
+            <Button className='loginbutton' type="submit"
                 style={{borderColor: "#353535", backgroundColor: "#353535"}}>
-                    Login
-                </Button>
-                <Button className='loginbutton' type="submit" href='/SignUp'
-                style={{borderColor: "#515151" ,backgroundColor: "#515151", marginRight: "5px"}}>
-                    SignUp
-                </Button>
-            </div>
-        </Form>
+                Login
+            </Button>
+        </div>
     )
 }
 
