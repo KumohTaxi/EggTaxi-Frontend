5bb2225ecf4a9db117f84b97e9f309609dc70fbb
GuHyeonWoo <ghw9174@gmail.com>
2022-04-03 19:37:11 +0900

    서버 통신 연결

--- src/forms/LoginForm.js
+++ src/forms/LoginForm.js
@@ -1,7 +1,28 @@
+import axios from 'axios';
 import {Button} from 'react-bootstrap';
 import './LoginForm.css';
 
 const LoginForm =()=>{
+
+    const L_function = () =>{
+        var setId = 'asd'
+        var setPw = 'asd'
+        console.log("asdasd");
+        axios({
+            method:'post',
+            url:'./start',
+            data:{id:setId, password:setPw},
+            headers:{
+                'ContentType':'appliction/json'
+            },
+        })
+        .then(() => {
+            alert("성공");
+        })
+        .catch(() => {
+            alert("실패");
+        })
+    }
   
     return(
         <div className='buttonGroup'>
@@ -9,7 +30,7 @@ const LoginForm =()=>{
                 style={{margin: "5px", borderColor: "#515151" ,backgroundColor: "#515151"}}>
                 SignUp
             </Button>
-            <Button className='loginbutton' type="submit"
+            <Button className='loginbutton' type="submit" onClick={L_function}
                 style={{borderColor: "#353535", backgroundColor: "#353535"}}>
                 Login
             </Button>
