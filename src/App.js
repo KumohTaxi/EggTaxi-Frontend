29881397890e37aad6b2af7cb188048920e915e4
GuHyeonWoo <ghw9174@gmail.com>
2022-04-09 12:29:36 +0900

    회원가입 페이지 삭제 및 MyModal 수정

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MainPage from './pages/MainPage.js';
 import LoginPage from './pages/LoginPage.js';
-import SignUpPage from './pages/SignUpPage.js';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import { LatLngContext } from './contexts/LatLngContexts';
 import React, { useState } from 'react';
@@ -19,7 +18,6 @@ function App() {
           <Router>
             <Route className= "LoginPage" path="/" component={LoginPage} exact={true}/>
             <Route className= "MainPage" path="/Main" component={MainPage}/>
-            <Route className= "SignUpPage" path="/SignUp" component={SignUpPage}/>
           </Router>
         </div>
       </MakeContext.Provider>
--- src/modals/MyModal.js
+++ src/modals/MyModal.js
@@ -9,18 +9,42 @@ const MyModal=(props)=>{
         aria-labelledby="contained-modal-title-vcenter"
         centered
         >
-            <Modal.Header closeButton>
-                <Modal.Title id="contained-modal-title-vcenter">
+            <Modal.Header style={{backgroundColor: "#282828"}}>
+                <Modal.Title className='MyTitle' id="contained-modal-title-vcenter">
                 My Room
                 </Modal.Title>
             </Modal.Header>
-            <Modal.Body>
-                <p>
-                본인의 방 정보입니다.
-                </p>
+
+            <Modal.Body className='MyMiddle'>
+                <div className= 'personBox'>
+                    <div className='mySubTitle'>
+                        1
+                    </div>
+                    <div className='mySubContent'>
+                        2
+                    </div>
+                </div>
+                <div className='timeBox'>
+                    <div className='mySubTitle'>
+                        3
+                    </div>
+                    <div className='mySubContent'>
+                        4
+                    </div>
+                </div>
+                <div className='destiBox'>
+                    <div className='mySubTitle'>
+                        5
+                    </div>
+                    <div className='mySubContent'>
+                        6
+                    </div>
+                </div>
             </Modal.Body>
-            <Modal.Footer>
-                <Button onClick={props.onHide}>Close</Button>
+
+            <Modal.Footer className='MyFooter' style={{backgroundColor: "#FFFCEE"}}>
+                <Button className='MyBottomButtonLeft'>방 삭제</Button>
+                <Button className='MyBottomButtonRight' onClick={props.onHide}>확인</Button>
             </Modal.Footer>
         </Modal>
     );
