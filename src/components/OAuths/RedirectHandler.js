f8e85caf3e1c7203c4995157a38b498dc36aa0ce
GuHyeonWoo <ghw9174@gmail.com>
2022-04-17 01:35:31 +0900

    accessToken, refreshToken localStorage에 저장

--- src/components/OAuths/RedirectHandler.js
+++ src/components/OAuths/RedirectHandler.js
@@ -6,9 +6,7 @@ import './RedirectHandler.css';
 const RedirectHandler = ({ history }) => {
     let code = new URL(window.location.href).searchParams.get("code");
 
-    useEffect(() => {   
-        console.log(code);
-        
+    useEffect(() => {
         axios({
             method: "POST",
             url: "/auth/kakao",
@@ -20,10 +18,19 @@ const RedirectHandler = ({ history }) => {
             },
         })
         .then((res)=>{
-            console.log(res);
+            console.log(res.data.accessToken);
+            console.log(res.data.refreshToken);
+            
+            const ACCESS_TOKEN = res.data.accessToken;
+            const REFRESH_TOKEN = res.data.refreshToken;
+
+            localStorage.setItem("access_token", ACCESS_TOKEN);
+            localStorage.setItem("refresh_token", REFRESH_TOKEN);
+
             history.push('/main');
         })
         .catch(()=>{
+            alert("로그인에 실패하였습니다.");
             history.push('/');
         })
     }, []);
