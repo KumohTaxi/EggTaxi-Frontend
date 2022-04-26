2460da272e4c8d2e7ffd2ec65d8b128620879147
GuHyeonWoo <ghw9174@gmail.com>
2022-04-27 01:18:14 +0900

    offcanvas 디자인, filter위치 재조정 필요

--- src/components/bars/Navbars.js
+++ src/components/bars/Navbars.js
@@ -1,4 +1,4 @@
-import {Navbar, Nav, Container, Offcanvas, NavDropdown, FormControl, Button, Form} from 'react-bootstrap';
+import {Navbar, Nav, Container, Offcanvas, NavDropdown, Dropdown} from 'react-bootstrap';
 import './Navbars.css';
 import React, {useState} from 'react'
 import UserModal from '../modals/UserModal.js'
@@ -10,29 +10,16 @@ const Navbars=()=>{
 
     return(
         <div>
-            {/* <Navbar bg="dark" variant="dark" style={{fontWeight: "bold"}}>
-                <Container id='container'>
-                    <Navbar.Brand>
-                        <img className='Logo' src="imgs/Taxi-Logo.png"
-                        style={{width: "60px", height: "60px"}}/>
-                    </Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link className='NavbarsMake' onClick={() => setCheckView(true)}>Make</Nav.Link>
-                        <Nav.Link className='NavbarsMyGroup' onClick={() => setUserView(true)}>MyGroup</Nav.Link>
-                    </Nav>
-                </Container>
-            </Navbar> */}
-
             <Navbar  bg="dark" variant="dark"  expand={false} style={{fontWeight: "bold"}}>
                 <Container fluid>
-                    <Navbar.Brand>
-                        <img className='Logo' src="imgs/Taxi-Logo.png"
-                        style={{width: "60px", height: "60px"}}/>
-                    </Navbar.Brand>
+                    <div>
+                        <Navbar.Brand className='NavTitle'>
+                            <img className='Logo' src="imgs/Taxi_Logo.png"
+                            style={{width: "65px", height: "23px"}}/>
+                        </Navbar.Brand>
 
-                    <Nav.Link className='NavbarsMake' onClick={() => setCheckView(true)}>Make</Nav.Link>
-                    <Nav.Link className='NavbarsMyGroup' onClick={() => setUserView(true)}>MyGroup</Nav.Link>
-
+                        <span className='NavbarsMake' onClick={() => setCheckView(true)}>Make Group</span>
+                    </div>
 
                     <Navbar.Toggle aria-controls="offcanvasNavbar" />
                     <Navbar.Offcanvas
@@ -40,23 +27,20 @@ const Navbars=()=>{
                     aria-labelledby="offcanvasNavbarLabel"
                     placement="end"
                     >
-                    <Offcanvas.Header closeButton>
-                        <Offcanvas.Title id="offcanvasNavbarLabel">Offcanvas</Offcanvas.Title>
-                    </Offcanvas.Header>
-                    <Offcanvas.Body>
-                        <Nav className="justify-content-end flex-grow-1 pe-3">
-                        <Nav.Link href="#action1">Home</Nav.Link>
-                        <Nav.Link href="#action2">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="offcanvasNavbarDropdown">
-                            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action5">
-                            Something else here
-                            </NavDropdown.Item>
-                        </NavDropdown>
-                        </Nav>
-                    </Offcanvas.Body>
+                        <Offcanvas.Header closeButton>
+                            <Offcanvas.Title id="offcanvasNavbarLabel" style={{fontWeight: "bold"}}>MENU</Offcanvas.Title>
+                        </Offcanvas.Header>
+                        <Offcanvas.Body >
+                            <Nav className="justify-content-end flex-grow-1 pe-3">
+                                <Nav.Link href="#action1" style={{color: "rgb(185, 185, 185)"}}>My Page</Nav.Link>
+                                <Nav.Link onClick={() => setUserView(true)} style={{color: "rgb(185, 185, 185)"}}>My Group</Nav.Link>
+                                <NavDropdown id="offcanvasNavbarDropdown" title={"Filter"}>
+                                    <NavDropdown.Item href="#action3">#1</NavDropdown.Item>
+                                    <NavDropdown.Divider />
+                                    <NavDropdown.Item href="#action4">#2</NavDropdown.Item>
+                                </NavDropdown>
+                            </Nav>
+                        </Offcanvas.Body>
                     </Navbar.Offcanvas>
                 </Container>
             </Navbar>
