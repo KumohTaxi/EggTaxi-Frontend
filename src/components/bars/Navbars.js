609a87709fd645fdfd63162ee6be63768eec5ba0
GuHyeonWoo <ghw9174@gmail.com>
2022-04-27 14:50:28 +0900

    필터 canvas 좌측으로 설정

--- src/components/bars/Navbars.js
+++ src/components/bars/Navbars.js
@@ -8,40 +8,52 @@ const Navbars=()=>{
     const [checkView, setCheckView] = useState(false);
     const [userView, setUserView] = useState(false);
 
+    const [leftCanvasShow, setLeftCanvasShow] = useState(false);
+    const lefthandleClose = () => setLeftCanvasShow(false);
+    const lefthandleShow = () => setLeftCanvasShow(true);
+
+    const [rightCanvasShow, setRightCanvasShow] = useState(false);
+    const righthandleClose = () => setRightCanvasShow(false);
+    const righthandleshow = () => setRightCanvasShow(true);
+
     return(
         <div>
             <Navbar  bg="dark" variant="dark"  expand={false} style={{fontWeight: "bold"}}>
                 <Container fluid>
                     <div>
                         <Navbar.Brand className='NavTitle'>
-                            <img className='Logo' src="imgs/Taxi_Logo.png"
-                            style={{width: "65px", height: "23px"}}/>
+                            <img className='Logo' src="imgs/Taxi_Logo.png"/>
                         </Navbar.Brand>
 
-                        <span className='NavbarsMake' onClick={() => setCheckView(true)}>Make Group</span>
+                        <span className='NavbarsMake' onClick={() => setCheckView(true)}>MakeGroup</span>
+                        <span className='NavbarsFilter' onClick={lefthandleShow}>Filter</span>
                     </div>
-
-                    <Navbar.Toggle aria-controls="offcanvasNavbar" />
-                    <Navbar.Offcanvas
-                    id="offcanvasNavbar"
-                    aria-labelledby="offcanvasNavbarLabel"
-                    placement="end"
-                    >
+                    
+                    <Navbar.Toggle onClick={righthandleshow}/>
+                    <Offcanvas show={rightCanvasShow} onHide={righthandleClose} className="offcanvasNavbar" placement='end'>
                         <Offcanvas.Header closeButton>
-                            <Offcanvas.Title id="offcanvasNavbarLabel" style={{fontWeight: "bold"}}>MENU</Offcanvas.Title>
+                            <Offcanvas.Title id="offcanvasNavbarLabel" style={{fontWeight: "bold"}}>MENU</Offcanvas.Title>                        
                         </Offcanvas.Header>
-                        <Offcanvas.Body >
+                        <Offcanvas.Body>
                             <Nav className="justify-content-end flex-grow-1 pe-3">
                                 <Nav.Link href="#action1" style={{color: "rgb(185, 185, 185)"}}>My Page</Nav.Link>
                                 <Nav.Link onClick={() => setUserView(true)} style={{color: "rgb(185, 185, 185)"}}>My Group</Nav.Link>
-                                <NavDropdown id="offcanvasNavbarDropdown" title={"Filter"}>
-                                    <NavDropdown.Item href="#action3">#1</NavDropdown.Item>
-                                    <NavDropdown.Divider />
-                                    <NavDropdown.Item href="#action4">#2</NavDropdown.Item>
-                                </NavDropdown>
                             </Nav>
                         </Offcanvas.Body>
-                    </Navbar.Offcanvas>
+                    </Offcanvas>
+
+                    <Offcanvas show={leftCanvasShow} onHide={lefthandleClose} className="filterOffCanvas" placement='start'>
+                        <Offcanvas.Header closeButton>
+                            <Offcanvas.Title id="offcanvasNavbarLabel" style={{fontWeight: "bold"}}>Filter</Offcanvas.Title>                        
+                        </Offcanvas.Header>
+                        <Offcanvas.Body>
+                            <NavDropdown id="offcanvasNavbarDropdown" title={"Filter"}>
+                                <NavDropdown.Item href="#action3">#1</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action4">#2</NavDropdown.Item>
+                            </NavDropdown>
+                        </Offcanvas.Body>
+                    </Offcanvas>
                 </Container>
             </Navbar>
 
