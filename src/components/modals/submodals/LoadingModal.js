f2b3f8e5fbd1a3e08368c159771d8d93d1fa7834
GuHyeonWoo <ghw9174@gmail.com>
2022-05-07 21:24:30 +0900

    본인의 위치 정보를 받아오기 전까지 Loading modal 출력

---
+++ src/components/modals/submodals/LoadingModal.js
@@ -0,0 +1,32 @@
+import {Modal, Spinner, Button} from 'react-bootstrap';
+import './LoadingModal.css';
+
+const LoadingModal=(props)=>{
+    return(
+        <Modal
+        {...props}
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        className='box'
+        backdrop="static"
+        >
+            <Modal.Body className='LoadingMiddle'>
+                <Spinner className="loadingSpinner" animation="border"/>
+                <p className='LoadingP1'>
+                    위치를 받아오는 중입니다...
+                </p>
+                <p className='LoadingP2'>
+                    시간이 지연된다면 네트워크 혹은
+                </p>
+                <p className='LoadingP3'>
+                    브라우저의 '위치 액세스 허용'을 확인 해주세요.
+                </p>
+                <Button className='LoadingButton' variant='dark' onClick={props.onHide}>
+                    직접 위치 찾기
+                </Button>
+            </Modal.Body>
+        </Modal>
+    );
+}
+
+export default LoadingModal;
