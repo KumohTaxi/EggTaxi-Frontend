c131fe921e1b019dcad957c8fa27f2333170553e
GuHyeonWoo <ghw9174@gmail.com>
2022-05-07 23:30:47 +0900

    LoginPage 디자인 수정

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -5,20 +5,12 @@ import LoginForm from "../forms/LoginForm";
 const LoginPage=()=>{
 
     return(
-        <div id='Login'>
-            <div id="loginmain">
-                <div className="logintop">
-                    <img className='loginLogo' src="imgs/ET_Logo.png" alt="이미지를 불러오지 못했습니다."/>
-                </div>
-                <div className="loginmiddle">
-                    <div className="LoginForm">
-                        {<LoginForm/>}
-                    </div>
-                </div>
+        <div id="loginmain">
+            <img className="loginLogo" src="imgs/ET_Logo.png" alt="이미지를 불러오지 못했습니다."/>
+            <div className="LoginForm">
+                {<LoginForm/>}
             </div>
-            <div className='Footer'>
-                {<Footer/>}
-            </div>
+            <img className="loginCompanyName" src="imgs/company_name_white.png" alt="이미지를 불러오지 못했습니다."/>
         </div>
     );
 }
