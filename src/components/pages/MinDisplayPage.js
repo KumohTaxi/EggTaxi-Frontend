8fda69bc03b36e154e0ebfc79d148d37f42e155e
GuHyeonWoo <ghw9174@gmail.com>
2022-05-07 21:26:52 +0900

    화면이 일정 수준 작아지면 MinDisplay Page 출력

---
+++ src/components/pages/MinDisplayPage.js
@@ -0,0 +1,11 @@
+import './MinDisplayPage.css';
+
+const MinDisplayPage=()=> {
+    return(
+        <div className="MDPage">
+            해상도를 높여주세요.
+        </div>
+    );
+}
+
+export default MinDisplayPage;
