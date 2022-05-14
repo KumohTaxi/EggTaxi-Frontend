c9f52278a6bf2fcea6cbde1dfac1b213067627af
GuHyeonWoo <ghw9174@gmail.com>
2022-05-15 06:10:47 +0900

    브라우저 주소창 크기 적용

--- src/components/modals/submodals/GuideModal.js
+++ src/components/modals/submodals/GuideModal.js
@@ -3,6 +3,15 @@ import './GuideModal.css';
 import { useEffect, useState } from 'react';
 
 const GuideModal=(props)=>{
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+    window.addEventListener("resize", () => {
+        console.log("resize");
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty("--vh", `${vh}px`);
+    });
+
     const [isCheck, setIsCheck] = useState(true);
 
     function Checked(){
