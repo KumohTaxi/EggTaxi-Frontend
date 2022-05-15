8a2f027f459ac7e5d55bc445a47babb0002de387
GuHyeonWoo <ghw9174@gmail.com>
2022-05-16 00:14:31 +0900

    로그인 안하고 main 페이지 넘어가면 다시 로그인 페이지로 이동

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -2,6 +2,7 @@ import Navbars from "../bars/Navbars";
 import Map from "../maps/Map";
 import Footer from "../footers/Footer";
 import './MainPage.css';
+import { useEffect } from "react";
 
 const MainPage=()=>{
     let vh = window.innerHeight * 0.01;
@@ -12,6 +13,12 @@ const MainPage=()=>{
         let vh = window.innerHeight * 0.01;
         document.documentElement.style.setProperty("--vh", `${vh}px`);
     });
+
+    useEffect(()=>{
+        if(localStorage.getItem("access_token") === null){
+            window.location.replace('/');
+        }
+    }, [])
 
     return(
         <div id='Main'>
