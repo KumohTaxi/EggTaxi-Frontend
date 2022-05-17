69ad0907a27297702b2e5638ecbbd31035dcfe31
GuHyeonWoo <ghw9174@gmail.com>
2022-05-17 15:58:02 +0900

    promotion 페이지 ux/ui 수정

--- src/components/pages/PromotionPage.js
+++ src/components/pages/PromotionPage.js
@@ -3,125 +3,144 @@ import { useEffect, useState } from 'react';
 import { KAKAO_AUTH_URL } from '../../oauths/OAuth';
 import { PROXY } from '../../contexts/ProxyContext';
 import axios from 'axios';
-import {Modal, Button, Badge} from 'react-bootstrap';
+import {Button} from 'react-bootstrap';
+import PromotionMap from '../maps/PromotionMap';
 
 const PromotionPage=()=> {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+    window.addEventListener("resize", () => {
+        console.log("resize");
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty("--vh", `${vh}px`);
+    });
+
     const [isMemberCount, setIsMemberCount] = useState();
     const [isMonth, setIsMonth] = useState();
     const [isDay, setIsDay] = useState();
     const [isHour, setIsHour] = useState();
     const [isMinute, setIsMinutes] = useState();
     const [isDestination, setIsDestination] = useState();
+    const [isLat, setIsLat] = useState();
+    const [isLng, setIsLng] = useState();
 
     useEffect(()=>{
-        localStorage.setItem("promotion", true);
-        localStorage.setItem("promotionLink", document.location.href);
-
         if(localStorage.getItem("access_token") === null){
-            alert('로그인 정보가 없습니다. 카카오톡 로그인으로 이동합니다.')
-            window.location.href= KAKAO_AUTH_URL;
+            localStorage.setItem("promotion", true);
+            localStorage.setItem("promotionLink", document.location.href);
         }
-        else{
-            localStorage.removeItem("promotion");
-            localStorage.removeItem("promotionLink");
-
-            axios({
-                method:'get',
-                url:`${PROXY}/group/${window.location.pathname[11]}/link`,
-                headers:{
-                    'ContentType':'application/json'
-                },
-            })
-            .then((res) => {
-                setIsMemberCount(res.data.memberCount);
-                setIsMonth(res.data.dateTime[5]+res.data.dateTime[6]);
-                setIsDay(res.data.dateTime[8]+res.data.dateTime[9]);
-                setIsHour(res.data.dateTime[11]+res.data.dateTime[12]);
-                setIsMinutes(res.data.dateTime[14]+res.data.dateTime[15]);
-                setIsDestination(res.data.destination);
-            })
-        }
+        axios({
+            method:'get',
+            url:`${PROXY}/group/${window.location.pathname[11]}`,
+            headers:{
+                'ContentType':'application/json'
+            },
+        })
+        .then((res) => {
+            setIsMemberCount(res.data.memberCount);
+            setIsMonth(res.data.dateTime[5]+res.data.dateTime[6]);
+            setIsDay(res.data.dateTime[8]+res.data.dateTime[9]);
+            setIsHour(res.data.dateTime[11]+res.data.dateTime[12]);
+            setIsMinutes(res.data.dateTime[14]+res.data.dateTime[15]);
+            setIsDestination(res.data.destination);
+            setIsLat(res.data.latitude);
+            setIsLng(res.data.longitude);
+        })
     }, [])
 
     function Cancel(){
         window.location.replace('/');
     }
+    function CheckAgree(){
+        if(localStorage.getItem("access_token") === null){
+            if(window.confirm("로그인이 필요한 서비스입니다.\n로그인 페이지로 이동하겠습니까?") == true){
+                window.location.replace('/');
+            }
+        }
+        else{
+            localStorage.removeItem("promotion");
+            localStorage.removeItem("promotionLink");
+            Agree();
+        }
+    }
     function Agree(){
-        axios({
-            method:'post',
-            url:`${PROXY}/./group/${window.location.pathname[11]}`,
-            data:{
-                accessToken: localStorage.getItem('access_token'),
-            },
-            headers:{
-                'ContentType':'application/json'
-            },
-        })
-        .then(() => {
+        if(localStorage.getItem("mygroupid")){
+            alert("이미 참가한 방이 있습니다.");
             window.location.replace('/main');
-        })
-        .catch(() => {
-            alert("참가 할 수 없는 방입니다.");
-            window.location.replace('/');
-        })
+        }
+        else{
+            axios({
+                method:'post',
+                url:`${PROXY}/./group/${window.location.pathname[11]}`,
+                data:{
+                    accessToken: localStorage.getItem('access_token'),
+                },
+                headers:{
+                    'ContentType':'application/json'
+                },
+            })
+            .then(() => {
+                window.location.replace('/main');
+            })
+            .catch(() => {
+                alert("참가 할 수 없는 방입니다.");
+                window.location.replace('/');
+            })
+        }
     }
 
     return(
         <div className='PromotionModal'>
-            <Modal
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-            show={true}
-            >
-                <Modal.Header style={{backgroundColor: "#212428"}}>
-                    <Modal.Title className='proTitle' id="contained-modal-title-vcenter">
-                    그룹 참가하기
-                    </Modal.Title>
-                </Modal.Header>
-
-                <Modal.Body className='proMiddle'>
-                    <div className='proscrollBox'>
-                        <div className= 'propersonBox'>
-                            <div className='proSubTitle'>
-                                <span>
-                                    인원
-                                </span>
-                            </div>
-                            <div className="vr" />
-                            <div className='proSubContent'>
-                                <Badge bg="light" text="dark">{isMemberCount} / 4명</Badge>
-                            </div>
-                        </div>
-                        <div className='prodestiBox'>
-                            <div className='proSubTitle'>
-                                <span>
-                                    목적지
-                                </span>
-                            </div>
-                            <div className="vr" />
-                            <div className='proSubContent'>
-                                <Badge bg="light" text="dark">{isDestination}</Badge>
-                            </div>
-                        </div>
-                        <div className='protimeBox'>
-                            <div className='proSubTitle'>
-                                <span>
-                                    출발시각
-                                </span>
-                            </div>
-                            <div className="vr" />
-                            <div className='proSubContent'>
-                                <Badge bg="light" text="dark">{isMonth}월 {isDay}일 &nbsp;/&nbsp; {isHour}시 {isMinute}분</Badge>
-                            </div>
-                        </div>
+            <div className='proTop'>
+                <div className='proInfo'>
+                    <div className='proInfoName'>
+                        인원
                     </div>
-                </Modal.Body>
-
-                <Modal.Footer className='proFooter' style={{backgroundColor: "#FFFCEE"}}>
-                    <Button variant="dark" className='proBottomButtonLeft' onClick={Cancel}>취소</Button>
-                    <Button variant="light" className='proBottomButtonRight' onClick={Agree}>참가</Button>
-                </Modal.Footer>
-            </Modal>
+                    <div className='proInfoContent'>
+                        <span className='proSpan'>
+                        {isMemberCount} / 4명
+                        </span>
+                    </div>
+                </div>
+                <div className='proInfo'> 
+                    <div className='proInfoName'>
+                        목적지
+                    </div>
+                    <div className='proInfoContent'>
+                        <span className='proSpan'>
+                        {isDestination}
+                        </span>
+                    </div>
+                </div>
+                <div className='proInfo'>
+                    <div className='proInfoName'>
+                        출발시간
+                    </div>
+                    <div className='proInfoContent'>
+                        <span className='proSpan'>
+                        {isMonth}월 {isDay}일 &nbsp;/&nbsp; {isHour}시 {isMinute}분
+                        </span>
+                    </div>
+                </div>
+                <div className='proInfoSub'>
+                ▼ 출발 위치 ▼
+                </div>
+            </div>
+            <div className='proMiddle'>
+                <PromotionMap 
+                proLat = {isLat}
+                proLng = {isLng}
+                />
+            </div>
+            <div className='proBottom'>
+                <Button variant='dark' className='proleftbutton' onClick={Cancel}>
+                    취소
+                </Button>
+                <Button variant='light' className='prorightbutton' onClick={CheckAgree}>
+                    그룹 참가
+                </Button>
+            </div>
         </div>
     );
 }
