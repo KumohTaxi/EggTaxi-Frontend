711453ea9cfc9316cdc50a6f774cc4ea3169d6fe
GuHyeonWoo <ghw9174@gmail.com>
2022-05-22 06:06:02 +0900

    버전 최신화

--- src/components/footers/Footer.js
+++ src/components/footers/Footer.js
@@ -4,7 +4,7 @@ const Footer=()=>{
 
     return(
         <div id='container'>
-            Egg-Taxi &nbsp;&nbsp;&nbsp;&nbsp; v1.0.2-beta
+            Egg-Taxi &nbsp;&nbsp;&nbsp;&nbsp; v1.0.3-beta
         </div>
     )
 }
