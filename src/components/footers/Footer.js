896d835051bfedc14badca955dde8af13ef2e0d2
GuHyeonWoo <ghw9174@gmail.com>
2022-05-22 00:03:15 +0900

    footer 디자인 추가

--- src/components/footers/Footer.js
+++ src/components/footers/Footer.js
@@ -1,21 +1,10 @@
 import './Footer.css';
 
 const Footer=()=>{
+
     return(
         <div id='container'>
-            <div id="left">
-                <div id="top">
-                    <img className='companyName' src='imgs/company_name_white.png'/>
-                </div>
-            </div>
-            <div id="right">
-                <div id="top">
-                    Front-end 신영한, 구현우
-                </div>
-                <div id="bottom">
-                    Back-end 김성은
-                </div>
-            </div>
+            Egg-Taxi &nbsp;&nbsp;&nbsp;&nbsp; v1.0.2-beta
         </div>
     )
 }
