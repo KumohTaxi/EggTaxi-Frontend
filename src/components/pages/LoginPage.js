96480d85b412a1a273b29f25323f7d5ce0edd627
GuHyeonWoo <ghw9174@gmail.com>
2022-05-22 00:04:03 +0900

    로그인 페이지 ui/ux 수정

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -14,6 +14,7 @@ const LoginPage=()=>{
     });
 
     const [isGuide, setIsGuide] = useState(true);
+    const madebyHref = window.location.href + 'madeby';
 
     useEffect(()=>{
         if(localStorage.getItem('guide_check') === null){
@@ -23,11 +24,27 @@ const LoginPage=()=>{
 
     return(
         <div id="loginmain">
-            <img className="loginLogo" src="imgs/ET_Logo.png" alt="이미지를 불러오지 못했습니다."/>
+            <div className='loginHeader'>
+                <img className="loginLogo" src="imgs/ET_Logo.png" alt="이미지를 불러오지 못했습니다."/>
+            </div>
             <div className="LoginForm">
                 {<LoginForm/>}
             </div>
-            <img className="loginCompanyName" src="imgs/company_name_white.png" alt="이미지를 불러오지 못했습니다."/>
+            <div className='loginFooter'>
+                <div className='collaboLogo'>
+                    <img className="loginCompanyName" src="imgs/company_name_white.png" alt="이미지를 불러오지 못했습니다."/>
+                    <img className='xLogo' src='imgs/Close.png' alt='이미지를 불러오지 못했습니다.'/>
+                    <img className='likelionLogo' src='imgs/likelion.png' alt='이미지를 불러오지 못했습니다.'/>
+                </div>
+                <div className='appInfo'>
+                    <div className='version'>
+                        v1.0.2-beta
+                    </div>
+                    <div className='madeby' onClick={()=>{window.location.href = madebyHref;}}>
+                        made by
+                    </div>
+                </div>
+            </div>
 
             <GuideModal
                 show = {isGuide}
