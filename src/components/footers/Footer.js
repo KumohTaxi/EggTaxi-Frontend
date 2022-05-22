b7b76066ea09143f23264899c48f8fdc017e9f1a
GuHyeonWoo <ghw9174@gmail.com>
2022-05-22 21:57:05 +0900

    v1.0.10

--- src/components/footers/Footer.js
+++ src/components/footers/Footer.js
@@ -4,7 +4,7 @@ const Footer=()=>{
 
     return(
         <div id='container'>
-            Egg-Taxi &nbsp;&nbsp;&nbsp;&nbsp; v1.0.9-beta
+            Egg-Taxi &nbsp;&nbsp;&nbsp;&nbsp; v1.0.10-beta
         </div>
     )
 }
