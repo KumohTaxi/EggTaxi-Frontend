b1a42d5a77639e60a4954258cc24a15fc79ff625
GuHyeonWoo <ghw9174@gmail.com>
2022-05-29 18:14:08 +0900

    app 설치 가이드 모달 추가

---
+++ src/components/modals/submodals/guide_modals/AppGuideModal.js
@@ -0,0 +1,25 @@
+import {Modal, CloseButton} from 'react-bootstrap';
+import './AppGuideModal.css';
+
+const AppGuideModal=(props)=>{
+    return(
+        <Modal
+        {...props}
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        >
+            <Modal.Header style={{backgroundColor: "#212428"}}>
+                <Modal.Title className='AppGuideTitle' id="contained-modal-title-vcenter">
+                App Install
+                </Modal.Title>
+                <CloseButton variant='white' onClick={props.onHide}/>
+            </Modal.Header>
+
+            <Modal.Body className='AppGuideMiddle'>
+                <img src='imgs/App_Guide.png' alt='이미지를 불러오지 못했습니다.'/>
+            </Modal.Body>
+        </Modal>
+    );
+}
+
+export default AppGuideModal;
