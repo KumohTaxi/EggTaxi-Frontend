3ca87c1b04d6f2b8e050bb1a5b72184d0fcf36fd
GuHyeonWoo <ghw9174@gmail.com>
2022-06-02 14:13:48 +0900

    v1.3.16

--- src/components/footers/Footer.js
+++ src/components/footers/Footer.js
@@ -4,7 +4,7 @@ const Footer=()=>{
 
     return(
         <div id='container'>
-            Egg-Taxi &nbsp;&nbsp;&nbsp;&nbsp; v1.2.16-beta
+            Egg-Taxi &nbsp;&nbsp;&nbsp;&nbsp; v1.3.16-beta
         </div>
     )
 }
--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -29,7 +29,7 @@ const LoginPage=()=>{
                 </div>
                 <div className='appInfo'>
                     <div className='version'>
-                        v1.2.16-beta
+                        v1.3.16-beta
                     </div>
                     <div className='madeby' onClick={()=>{window.location.href = madebyHref;}}>
                         made by
