2177e7f4f2fe1bd14e9ec174eac4c5f7a771590f
GuHyeonWoo <ghw9174@gmail.com>
2022-09-19 15:37:30 +0900

    fixed ustracked files

--- src/egg-taxi/contexts/ProxyContext.js
+++
@@ -1,3 +0,0 @@
-const PROXY_URI = "http://115.85.181.24:8081";
-
-export const PROXY = PROXY_URI
--- src/egg-taxi/oauths/ReAuth.js
+++
@@ -1,4 +0,0 @@
-const CLIENT_ID = "a613c56213a9e4aab0ef8cf3b5400dfd";
-const REDIRECT_URI = "http://localhost:3000/oauth/callback/kakao";
-
-export const GENDER_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=gender`;
