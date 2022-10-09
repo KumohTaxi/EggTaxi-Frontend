07f577185238b9d4d9117ed7aeb0dda144333ea9
GuHyeonWoo <ghw9174@gmail.com>
2022-10-10 02:30:31 +0900

    commenting

--- src/bsd/components/pages/BSDChatRoomPage.js
+++ src/bsd/components/pages/BSDChatRoomPage.js
@@ -3,16 +3,24 @@ import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
 const BSDChatRoomPage=()=>{
+    // useParams를 통해 채팅방 id를 확인
     const {id} = useParams();
 
+    // 임시 데이터
     const chatData = [
         {
+            // 채팅방 아이디
             chatid: 1,
+            // 상대방 이름
             opponent: '타조알',
+            // 채팅 기록
             chating: [
                 {
+                    // 보낸 사람의 상태
                     sender: 'oppon',
+                    // 채팅 내용
                     contents: '안녕하세요.',
+                    // 채팅 전송 시간대
                     date: '18:30'
                 },
                 {
@@ -146,11 +154,14 @@ const BSDChatRoomPage=()=>{
         }
     ]
 
+    // 채팅방의 채팅 기록을 저장할 useState
     const [chatObj, setChatObj] = useState('');
 
     useEffect(()=>{
+        // 채팅기록이 ''라면, 즉 초기 상태라면
         if(chatObj === ''){
             let temp;
+            // 채팅 데이터 중 useParams id와 일치하는 객체를 chatObj에 저장
             chatData.map(x=>{
                 if(x.chatid === parseInt(id)) temp = {...x};
             })
@@ -158,15 +169,20 @@ const BSDChatRoomPage=()=>{
         }
     },[])
 
+    // 채팅 기록을 반환하는 함수
     const showChatRecords = () => {
+        // 만약 채팅 기록을 저장하는 chatObj이 존재한다면
         if(chatObj.chating){
+            // 키 값을 저장하는 변수
             let keynum = 0;
+            // chatRecords 배열에 chatObj의 채팅 배열인 chating을 시간 기준 내림차순으로 정렬
             let chatRecords = chatObj.chating.sort((a, b)=>{
                 const adate = a.date.split(':').map(Number);
                 const bdate = b.date.split(':').map(Number);
                 if(adate[0] !== bdate[0]) return bdate[0] - adate[0];
                 else return bdate[1] - adate[1];
-            }).map(x=>{
+            }) // 정렬한 배열을 순회하면서 sender을 확인하여 본인 채팅과 상대방 채팅의 css를 구별
+            .map(x=>{
                 if(x.sender === 'me'){
                     return <div key={keynum++} className='bsd_chat_me'>
                         <div className='bsd_chat_date'>{x.date}</div>
--- src/bsd/components/pages/BSDChatingPage.js
+++ src/bsd/components/pages/BSDChatingPage.js
@@ -4,14 +4,21 @@ import { useNavigate } from 'react-router-dom'
 const BSDChatingPage = () => {
     const navi = useNavigate();
 
+    // 임시데이터
     const chatData = [
         {
+            // 채팅방 아이디
             chatid: 1,
+            // 상대방 이름
             opponent: '타조알',
+            // 채팅 기록
             chating: [
                 {
+                    // 보낸 사람의 상태
                     sender: 'oppon',
+                    // 채팅 내용
                     contents: '안녕하세요.',
+                    // 채팅 전송 시간대
                     date: '18:30'
                 },
                 {
@@ -65,21 +72,31 @@ const BSDChatingPage = () => {
         }
     ]
 
+    // 채팅 방 여는 함수
     const openChatRoom = (id) => {
+        // 모바일인디 데스크탑인지 판단
         const checkDevice = navigator.userAgent;
         let isMobile = false;
+        // 모바일이면 true
         if(checkDevice.indexOf("iPhone") > -1 || checkDevice.indexOf("Android") > -1) isMobile = true;
-
+        // 모바일이면 현재 창에서 이동
         if(isMobile) navi(`/bsd/chat/${id}`);
+        // 데스크탑이면 새로운 창 생성
         else window.open(`/bsd/chat/${id}`, `_blank`, `height=600, width=350, menubar=0, resizable=0 left=200 top=100`, `false`);
     }
 
+    // 채팅방 목록 불러오기
     const chatList = () => {
+        // 반환할 채팅방 배열
         let chatlist = [];
+        // 메세지가 가장 최근인 방을 상단에 띄우기 위해 정렬을 할 배열
         let sortChat = [];
 
+        // 임시 데이터를 순회 
         for (let i = 0; i < chatData.length; i++) {
+            // 임시 데이터에서 채팅 기록을 저장
             let tempChat = chatData[i].chating;
+            // chatid(채팅방 아이디), opponent(상대방 이름), recentText(최근 채팅 내용), recentDate(최근 채팅 날짜) 저장
             sortChat.push(
                 {
                     chatid: chatData[i].chatid,
@@ -89,14 +106,18 @@ const BSDChatingPage = () => {
                 }
             );
         }
+        // 저장한 정보를 시간을 기준으로 최신순으로 정렬
         sortChat.sort((a, b) => {
             let aTemp = a.recentDate.split(':').map(Number);
             let bTemp = b.recentDate.split(':').map(Number);
             
+            // 시간이 다르면 시간으로 내림차순
             if(aTemp[0] !== bTemp[0]) return bTemp[0] - aTemp[0];
+            // 시간이 같으면 분으로 내림차순
             else return bTemp[1] - aTemp[1];
         })
 
+        // 정렬된 배열을 순회하며 반환할 배열에 저장
         for (let i = 0; i < sortChat.length; i++) {
             chatlist.push(
                 <div key={i} className="chatRoom" onClick={()=>openChatRoom(sortChat[i].chatid)}>
