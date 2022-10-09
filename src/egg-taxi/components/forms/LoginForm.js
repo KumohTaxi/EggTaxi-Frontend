a9f202f559950c593a51909dcfe36810ab2c9138
GuHyeonWoo <ghw9174@gmail.com>
2022-10-09 13:46:54 +0900

    fixed button ui issue

--- src/egg-taxi/components/forms/LoginForm.js
+++ src/egg-taxi/components/forms/LoginForm.js
@@ -12,9 +12,10 @@ const LoginForm =()=>{
             <a href={KAKAO_AUTH_URL}>
                 <img src='imgs/kakao_login_large_wide.png' alt='이미지를 불러오지 못했습니다.'></img>
             </a>
-            <Button className='AppDownload' onClick={()=>{setIsAppGuide(true)}}>
+            <button className='AppDownload' onClick={()=>{setIsAppGuide(true)}}
+            style={{backgroundColor: '#396992'}}>
                 App 설치 가이드
-            </Button>
+            </button>
 
             <AppGuideModal
                 show = {isAppGuide}
