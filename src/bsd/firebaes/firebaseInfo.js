63347c95ec94d5bd0f58ff9ca8fbd9e60e72cd42
syhan7516 <syhan7516@naver.com>
2022-10-10 21:33:08 +0900

    firebase setting

---
+++ src/bsd/firebaes/firebaseInfo.js
@@ -0,0 +1,23 @@
+// Get the required features from the SDK
+
+// Import to use firebase initial
+import { initializeApp } from "firebase/app";
+// Import to use firestore
+import { getFirestore } from "firebase/firestore";
+
+// Web firebase component and SDK
+const firebaseConfig = {
+  apiKey: `${process.env.REACT_APP_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
+  projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_APP_ID}`
+};
+
+// Crate obj & Initialize firebase  
+const app = initializeApp(firebaseConfig);
+// Create firestore obj
+const dbService = getFirestore(app);
+// Export firebase, firestore
+export { app, dbService };
