c340678f77942b9c53617931df3471f2afdaf6a1
GuHyeonWoo <ghw9174@gmail.com>
2022-10-12 23:56:50 +0900

    delete unnecessary data

--- src/bsd/components/lists/SearchList.js
+++ src/bsd/components/lists/SearchList.js
@@ -1,36 +1,15 @@
 import '../../styles/lists/SearchList.css';
-import { React, useEffect, useState } from 'react';
-import axios from 'axios';
+import { React } from 'react';
 import Item from './Item';
 
-const SearchList = ({isFind}) => {
-
-    const [itemsInfo,setItemsInfo] = useState([]);
-
-    const getFoundItems =  async () => {        
-        const itemArray = await axios.get("https://jsonplaceholder.typicode.com/posts");
-        setItemsInfo(itemArray.data);
-    }
-
-    const getLostItems =  async () => {        
-        const itemArray = await axios.get("https://jsonplaceholder.typicode.com/albums");
-        setItemsInfo(itemArray.data);
-    }
-
-    useEffect(()=>{
-        {isFind ? 
-            getFoundItems() :
-            getLostItems();
-        }
-    },[isFind])
-
+const SearchList = ({isFind, isUseData}) => {
     return (
         <div id="lists" style={{height:"100%"}}> 
-            {itemsInfo.length === 0 ?
+            {isUseData.length === 0 ?
                 (<div className="item-notice-box">
-                    <div className="item-notice"> 등록된 {isFind ? "습득물" : "분실물"}이 없습니다 .. 흐엥 </div>
+                    <div className="item-notice"> 등록된 {isFind ? "습득물" : "분실물"}이 없습니다.</div>
                 </div>) :
-                (itemsInfo.map((itemInfo)=>{return <div key="itemInfo.id"><Item itemInfo={itemInfo}/></div>}))}
+                (isUseData.map((itemInfo, idx)=>{return <div key={idx}><Item itemInfo={itemInfo}/></div>}))}
         </div>
     )
 }
