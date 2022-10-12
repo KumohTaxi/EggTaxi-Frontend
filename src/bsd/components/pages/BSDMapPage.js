b0581319fa3b56afaa842d45be105963f08bc6fa
GuHyeonWoo <ghw9174@gmail.com>
2022-10-13 00:00:01 +0900

    implemented filtering data set

--- src/bsd/components/pages/BSDMapPage.js
+++ src/bsd/components/pages/BSDMapPage.js
@@ -3,21 +3,110 @@ import GoogleMap from '../maps/GoogleMap';
 import BSDHeader from '../headers/BSDHeader.js';
 import SearchList from '../lists/SearchList';
 
-import { React, useState } from 'react';
+import { React, useEffect, useState } from 'react';
+import axios from 'axios';
 
-const BSDMapPage=()=>{
-
+const BSDMapPage = () => {
     const [onMap, setOnMap] = useState(true);
     const [isFind, setIsFind] = useState(true);
+    const [InitData, setInitData] = useState([]);
+    const [isUseData, setIsUseData] = useState([]);
+    const [showCondition, setShowCondition] = useState({
+        current: true,
+        allofDate: true,
+        category: '',
+        name: ''
+    });
 
-    return(
+    const sortAry = (ary, current) => {
+        return ary.sort((a, b) => {
+            let aDate = parseInt(a.date.split('-').join(''));
+            let bDate = parseInt(b.date.split('-').join(''));
+            return current ? bDate - aDate : aDate - bDate;
+        });
+    }
+
+    useEffect(() => {
+        const setIsItems = (isItems) => {
+            let tempList = [];
+            if (isItems.length > 0) {
+                isItems.map(x => {
+                    const tempCategory = x.category.split(' ')[0];
+                    let temp = {
+                        atcId: x.atcId,
+                        category: tempCategory,
+                        imgSrc: x.img,
+                        address: x.location,
+                        date: x.lostDate,
+                        name: x.name
+                    };
+                    tempList.push(temp);
+                    return x;
+                })
+            }
+            setInitData(tempList);
+            setIsUseData(sortAry(tempList, true));
+        }
+
+        const status = isFind ? 'ACQUIRE' : 'LOST';
+
+        axios.get(`${process.env.REACT_APP_PROXY}/item?location=구미시&status=${status}`, {
+            headers: {}
+        })
+            .then((res) => {
+                setIsItems(res.data);
+            })
+            .catch((err) => {
+                alert('데이터를 불러오지 못했습니다. 새로고침을 해보세요.');
+            })
+    }, [isFind]);
+
+    useEffect(() => {
+        const filteringDate = (ary) => {
+            let filterList = [];
+            const tempMonth = showCondition.month < 10 ? '0' + showCondition.month : showCondition.month;
+            const tempDay = showCondition.day < 10 ? '0' + showCondition.day : showCondition.day;
+            let targetDate = showCondition.year + '-' + tempMonth + '-' + tempDay;
+            ary.map(x => {
+                if (x.date === targetDate) filterList.push(x);
+                return x;
+            })
+            return filterList;
+        }
+        const filteringCategory = (ary) => {
+            let filterList = [];
+            ary.map(x => {
+                if (x.category === showCondition.category) filterList.push(x);
+                return x;
+            })
+            return filterList;
+        }
+        const filteringName = (ary) => {
+            let filterList = [];
+            ary.map(x => {
+                if (x.name === showCondition.name) filterList.push(x);
+                return x;
+            })
+            return filterList;
+        }
+        
+        let filterList = InitData.slice();
+        if (showCondition.name !== '') filterList = filteringName(filterList);
+        if (showCondition.category !== '') filterList = filteringCategory(filterList);
+        if (!showCondition.allofDate) filterList = filteringDate(filterList);
+        filterList = sortAry(filterList, showCondition.current);
+
+        setIsUseData(filterList);
+    }, [InitData, showCondition]);
+
+    return (
         <div className='bsdMap'>
             <div className="map_header">
-                <BSDHeader onMap={onMap} setOnMap={setOnMap} isFind={isFind} setIsFind={setIsFind}/>
+                <BSDHeader onMap={onMap} setOnMap={setOnMap} isFind={isFind} setIsFind={setIsFind} setShowCondition={setShowCondition} />
             </div>
 
-            {onMap ? (<div className="map_map"><GoogleMap isFind={isFind}/></div>)
-            : (<div className="show_list"><SearchList isFind={isFind}/></div>)}
+            {onMap ? (<div className="map_map"><GoogleMap isFind={isFind} isUseData={isUseData} /></div>)
+                : (<div className="show_list"><SearchList isFind={isFind} isUseData={isUseData} /></div>)}
         </div>
     );
 }
