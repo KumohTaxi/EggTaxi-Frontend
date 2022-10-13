f4194559528ff40dc6b9bab7b749c1e66ecfcbc3
GuHyeonWoo <ghw9174@gmail.com>
2022-10-13 20:49:24 +0900

    finded issue

--- src/bsd/firebaes/firebaseInfo.js
+++ src/bsd/firebaes/firebaseInfo.js
@@ -1,23 +1,25 @@
-// Get the required features from the SDK
+// issu 해결 요망
 
-// Import to use firebase initial
-import { initializeApp } from "firebase/app";
-// Import to use firestore
-import { getFirestore } from "firebase/firestore";
+// // Get the required features from the SDK
 
-// Web firebase component and SDK
-const firebaseConfig = {
-  apiKey: `${process.env.REACT_APP_API_KEY}`,
-  authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
-  projectId: `${process.env.REACT_APP_PROJECT_ID}`,
-  storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
-  messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
-  appId: `${process.env.REACT_APP_APP_ID}`
-};
+// // Import to use firebase initial
+// import { initializeApp } from "firebase/app";
+// // Import to use firestore
+// import { getFirestore } from "firebase/firestore";
 
-// Crate obj & Initialize firebase  
-const app = initializeApp(firebaseConfig);
-// Create firestore obj
-const dbService = getFirestore(app);
-// Export firebase, firestore
-export { app, dbService };
+// // Web firebase component and SDK
+// const firebaseConfig = {
+//   apiKey: `${process.env.REACT_APP_API_KEY}`,
+//   authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
+//   projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+//   storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
+//   messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
+//   appId: `${process.env.REACT_APP_APP_ID}`
+// };
+
+// // Crate obj & Initialize firebase  
+// const app = initializeApp(firebaseConfig);
+// // Create firestore obj
+// const dbService = getFirestore(app);
+// // Export firebase, firestore
+// export { app, dbService };
