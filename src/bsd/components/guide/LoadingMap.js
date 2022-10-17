c74e35ce27bf87140dc6e9f300c7a3a4f1dba45d
GuHyeonWoo <ghw9174@gmail.com>
2022-10-17 13:51:17 +0900

    implemented marker loading window

--- src/bsd/components/guide/LoadingMap.js
+++ src/bsd/components/guide/LoadingMap.js
@@ -1,31 +1,31 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import '../../styles/guide/LoadingMap.css';
+import useInterval from '../../hooks/useInterval';
 
 const LoadingMap = () => {
-    const [loadText, setLoadText] = useState('데이터를 불러오는 중입니다');
-    const [textList] = useState(['.', '..', '...']);
-    const [idx, setIdx] = useState(0);
-    console.log(idx);
+    const [loadText] = useState('데이터를 불러오는 중입니다');
+    const [textList, setTextList] = useState('.');
 
-    useEffect(()=>{
-        setTimeout(()=>{
-            setIdx(idx++);
-            if(idx === 3) setIdx(0);
-        },100);
-    },[]);
+    const increaseIdx = () => {
+        if(textList==='.') setTextList('..');
+        else if(textList==='..') setTextList('...');
+        else setTextList('.');
+    }
+
+    useInterval(increaseIdx, 500);
 
     return (
         <div className="LoadingMap">
             <div>
                 <div>
-                    {loadText + textList[idx]}
+                    {loadText + textList}
                 </div>
                 <br/><br/><br/>
                 <div>
                     네트워크의 상태에 따라 로딩 지연이나,<br/>
                     데이터가 유실될 수 있습니다.<br/>
                     <br/>
-                    좀 더 질 좋은 데이터를 원한다면<br/>
+                    정확한 데이터를 원한다면<br/>
                     왼쪽 상단의 리스트로 보기 버튼을 선택해 주세요.
                 </div>
             </div>
