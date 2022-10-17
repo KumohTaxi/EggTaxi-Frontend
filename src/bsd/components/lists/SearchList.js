80171305e8b75d48f79ef03d74979a6636be5e3f
GuHyeonWoo <ghw9174@gmail.com>
2022-10-17 13:55:49 +0900

    implemented detail page linking

--- src/bsd/components/lists/SearchList.js
+++ src/bsd/components/lists/SearchList.js
@@ -1,16 +1,28 @@
 import '../../styles/lists/SearchList.css';
-import { React } from 'react';
+import { React, useState } from 'react';
 import Item from './Item';
+import DetailCanvas from '../offcanvases/DetailCanvas';
 
-const SearchList = ({isFind, isUseData}) => {
+const SearchList = ({ isFind, isUseData }) => {
+    const [detail, setDetail] = useState(false);
+    const [detailData, setDetailData] = useState({});
+
     return (
-        <div id="lists" style={{height:"100%"}}> 
-            {isUseData.length === 0 ?
-                (<div className="item-notice-box">
-                    <div className="item-notice"> 등록된 {isFind ? "습득물" : "분실물"}이 없습니다.</div>
-                </div>) :
-                (isUseData.map((itemInfo, idx)=>{return <div key={idx}><Item itemInfo={itemInfo}/></div>}))}
-        </div>
+        <>
+            <div id="lists" style={{ height: "100%" }}>
+                {isUseData.length === 0 ?
+                    (<div className="item-notice-box">
+                        <div className="item-notice"> 등록된 {isFind ? "습득물" : "분실물"}이 없습니다.</div>
+                    </div>) :
+                    (isUseData.map((itemInfo, idx) => { return <div key={idx}><Item itemInfo={itemInfo} setDetail={setDetail} setDetailData={setDetailData} /></div> }))}
+            </div>
+
+            <DetailCanvas
+                show={detail}
+                onHide={() => setDetail(false)}
+                itemInfo={detailData}
+            />
+        </>
     )
 }
 
--- src/bsd/components/offcanvases/DetailCanvas.js
+++ src/bsd/components/offcanvases/DetailCanvas.js
@@ -1,12 +1,73 @@
+import { useEffect, useState } from "react";
 import { Offcanvas } from "react-bootstrap";
 import '../../styles/offcanvases/DetailCanvas.css';
 
 const DetailCanvas = ({ show, onHide, itemInfo }) => {
-    if(show) console.log(itemInfo);
-    return (
-        <Offcanvas show={show} onHide={onHide} placement='bottom' style={{ borderRadius: "20px 20px 0 0", height: "80%" }}>
-            <Offcanvas.Body style={{width: "100%", height: "100%"}}>
+    const [showDetail, setShowDetail] = useState({});
 
+    console.log(showDetail);
+
+    useEffect(() => {
+        if (itemInfo.atcId === 'not police api data') {
+            setShowDetail({
+                imgSrc: itemInfo.imgSrc ? itemInfo.imgSrc : '/icons/bsd_null.png',
+                name: itemInfo.name,
+                category: itemInfo.category,
+                date: itemInfo.date,
+                registrant: 'nickname', // 닉네임. 경찰청인 경우 보관 장소
+                address: itemInfo.address,
+                comment: 'comment', // comment 정보도 받아올 것
+                contact: 'chat' // 경찰청인 경우 전화번호
+            });
+        }
+        else {
+            // 경찰청 상세 정보 받아오기
+        }
+    }, [itemInfo]);
+
+    const showDetailBottom = () => {
+        if (showDetail.contact === 'chat') {
+            return <button>질문에 답하기</button>
+        }
+        return <div>xxx-xxxx-xxxx</div>
+    }
+
+    return (
+        <Offcanvas show={show} onHide={onHide} placement='bottom' style={{ borderRadius: "20px 20px 0 0", height: "90%" }}>
+            <Offcanvas.Body style={{
+                width: "100%", height: "100%",
+                display: 'flex', flexDirection: 'column'
+            }}>
+                <div id="bsd_detail_top" onClick={() => onHide()}>
+                    닫기
+                </div>
+                <div id="bsd_detail_body">
+                    <div>
+                        {/* <img /> */}
+                        <div>
+                            <div>
+                                {showDetail.name}
+                            </div>
+                            <div>
+                                {showDetail.category} / {showDetail.date}
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            {showDetail.registrant}
+                        </div>
+                        <div>
+                            {showDetail.address}
+                        </div>
+                        <div>
+                            {showDetail.comment}
+                        </div>
+                    </div>
+                    <div id="bsd_detail_bottom">
+                        {showDetailBottom()}
+                    </div>
+                </div>
             </Offcanvas.Body>
         </Offcanvas>
     );
