fa34831df9307877313a4c3a4e0db061973df282
GuHyeonWoo <ghw9174@gmail.com>
2022-11-06 18:14:28 +0900

    chore gotoBSD: bsd 서비스 이동 제한 해제
    before: bsd 버튼 클릭 시 이동 제한
    after: bsd 버튼 클릭 시 /bsd/main으로 이동
    
    bsd 서비스 구현으로 인한 제한 해제

--- src/IndexPage.js
+++ src/IndexPage.js
@@ -8,8 +8,7 @@ function IndexPage() {
     navigate(`/taxi-login`);
   }
   const gotoBSD = () => {
-    alert('서비스 준비 중 입니다.');
-    // navigate(`/bsd/main`);
+    navigate(`/bsd/main`);
   }
 
   return (
